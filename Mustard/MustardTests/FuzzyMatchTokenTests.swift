11277a4bfad8c32106c3f638c40b7862b70386ad
Mathew Sanders <mathew.sanders@gmail.com>
2016-12-31 20:42:38 -0500

    Add fuzzy literal token test.

--- Mustard/MustardTests/FuzzyMatchTokenTests.swift
+++ Mustard/MustardTests/FuzzyMatchTokenTests.swift
@@ -11,12 +11,62 @@ import Mustard
 
 class FuzzyLiteralMatch: TokenType {
     
-    required init() { }
+    let target: String
+    let exclusions: CharacterSet
+    var position: String.UnicodeScalarIndex
     
-    init(target: String) { }
+    required convenience init() {
+        self.init(target: "", ignoring: CharacterSet.whitespaces)
+    }
+    
+    init(target: String, ignoring exclusions: CharacterSet) {
+        self.target = target
+        self.position = target.unicodeScalars.startIndex
+        self.exclusions = exclusions
+    }
     
     func canAppend(next scalar: UnicodeScalar) -> Bool {
-        return false
+        
+        guard position < target.unicodeScalars.endIndex else {
+            // we've matched all of the target
+            return false
+        }
+        
+        if scalar == target.unicodeScalars[position] {
+            // scalar matches the next target scalar!
+            // advance the position and return true
+            position = target.unicodeScalars.index(after: position)
+            return true
+        }
+        
+        else if position > target.unicodeScalars.startIndex && exclusions.contains(scalar) {
+            // if: 
+            // - we've matched at least one of the target scalars; and
+            // - this scalar matches a scalar that's can be ignored
+            // then:
+            // - return true without advancing the position
+            return true
+        }
+        else {
+            // scalar isn't the next target scalar, or a scalar that can be ignored
+            return false
+        }
+    }
+    
+    func canCompleteWhenNextScalar(is scalar: UnicodeScalar) -> Bool {
+        
+        if position == target.unicodeScalars.endIndex {
+            resetToken()
+            return true
+        }
+        else {
+            resetToken()
+            return false
+        }
+    }
+    
+    private func resetToken() {
+        position = target.unicodeScalars.startIndex
     }
 }
 
@@ -24,7 +74,11 @@ class FuzzyMatchTokenTests: XCTestCase {
     
     func testSpecialFormat() {
         
-        let tokens = "Serial: #YF 1942-B 12/01/27 (Scanned)".tokens(from: FuzzyLiteralMatch(target: "#YF1942B"))
+        let messyInput = "Serial: #YF 1942-B 12/01/27 (Scanned)"
+        let fuzzyTokenzier = FuzzyLiteralMatch(target: "#YF1942B",
+                                               ignoring: CharacterSet.whitespaces.union(.punctuationCharacters))
+        
+        let tokens = messyInput.tokens(from: fuzzyTokenzier)
         
         XCTAssert(tokens.count == 1, "Unexpected number of tokens [\(tokens.count)]")
         
